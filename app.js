5b0058c9367f815a6b37a381d93404b236977c12
Utori <utory_@naver.com>
2019-04-21 15:48:59 +0900

    저장기능 추가

--- app.js
+++ app.js
@@ -3,11 +3,17 @@ const ctx = canvas.getContext("2d");
 const colors = document.getElementsByClassName("jsColor");
 const range = document.getElementById("jsRange");
 const mode = document.getElementById("jsMode");
+const saveBtn = document.getElementById("jsSave");
+
+
 const INITILAL_COLOR = "#2c2c2c";
 const CANVAS_SIZE = 700;
 
 canvas.width = CANVAS_SIZE;
 canvas.height = CANVAS_SIZE;
+
+ctx.fillStyle = "white";
+ctx.fillRect(0, 0, canvas.width, canvas.height);
 
 ctx.strokeStyle = INITILAL_COLOR;
 ctx.fillStyle = INITILAL_COLOR;
@@ -64,12 +70,25 @@ function handleCanvasClick(){
     }
 }
 
+function handleSaveClick(){
+    const image = canvas.toDataURL();
+    const link = document.createElement("a");
+    link.href = image;
+    link.download = "PaintJS";
+    link.click();
+}
+
+function handleCM(event){
+    event.preventDefault();
+}
+
 if(canvas){
     canvas.addEventListener("mousemove",onMouseMove);
     canvas.addEventListener("mousedown",startPainting);
     canvas.addEventListener("mouseup",stopPainting);
     canvas.addEventListener("mouseleave",stopPainting);
     canvas.addEventListener("click",handleCanvasClick);
+    canvas.addEventListener("contextmenu",handleCM);
 }
 
 Array.from(colors).forEach(color => color.addEventListener("click",handleColorClick));
@@ -80,4 +99,9 @@ if(range){
 
 if(mode){
     mode.addEventListener("click", handleModeClick);
+}
+
+
+if(saveBtn){
+    saveBtn.addEventListener("click", handleSaveClick);
 }
